9b268f1488f88ab215734cff669c87ba65b7cad6
Maria Saavedra <msaav3@gmail.com>
2023-05-11 23:16:33 -0500

    chore: speed

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,33 +1,34 @@
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 
 import styles from './Hero.module.css';
 
-import AnimatedText from '@/components/AnimatedText/AnimatedText';
 import Viewer from '@/components/Viewer/Viewer';
+
+const AnimatedText = dynamic(() =>
+  import('@/components/AnimatedText/AnimatedText').then((mod) => mod)
+);
 
 export default function Hero() {
   return (
     <div className={styles.HeroComponent}>
-      {/* HOME */}
-      <div>
-        <div
-          className={
-            styles.bg +
-            ' relative  flex h-[65vh] w-screen overflow-hidden bg-black text-white'
-          }
-        >
-          <AnimatedText position='top'></AnimatedText>
-          <Viewer></Viewer>
-          <AnimatedText position='bottom'></AnimatedText>
+      <div
+        className={
+          styles.bg +
+          ' relative  flex h-[65vh] w-screen overflow-hidden bg-black text-white'
+        }
+      >
+        <AnimatedText position='top' />
+        <Viewer></Viewer>
+        <AnimatedText position='bottom' />
 
-          <Image
-            className='absolute bottom-20 right-5 z-10 invert md:filter-none'
-            src='/images/logo.svg'
-            width={120}
-            height={120}
-            alt=''
-          />
-        </div>
+        <Image
+          className='absolute bottom-20 right-5 z-10 invert md:filter-none'
+          src='/images/logo.svg'
+          width={120}
+          height={120}
+          alt=''
+        />
       </div>
     </div>
   );

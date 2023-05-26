7edc316fd1e6636f71c976d1dce5661e340fd9ec
Maria Saavedra <msaav3@gmail.com>
2023-05-26 02:07:40 -0500

    chore: added icons

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,5 +1,7 @@
 import styles from './Services.module.css';
 
+import SquiggleIcon from '@/components/SquiggleIcon/SquiggleIcon';
+
 export default function Services() {
   return (
     <div className={styles.ServicesComponent + ' bg-black p-16'}>
@@ -10,20 +12,49 @@ export default function Services() {
       <div className='font-secondary flex flex-col text-2xl font-thin uppercase leading-loose tracking-widest text-white md:flex-row '>
         <div className='rounded-lg md:w-1/2'>
           <ul>
-            <li className='border-b-2'>E-commerce SOLUTIONS</li>
-            <li className='border-b-2'>SHOPIFY & SQUARESPACE</li>
-            <li className='border-b-2'>THIRD-PARTY INTEGRATIONS</li>
-            <li className='border-b-2'>WebSITE Development</li>
-            <li className='border-b-2'>Custom Software</li>
+            <li className='border-b-2'>
+              <SquiggleIcon /> E-commerce SOLUTIONS
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              SHOPIFY & SQUARESPACE
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              THIRD-PARTY INTEGRATIONS
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              WebSITE Development
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              Custom Software
+            </li>
           </ul>
         </div>
         <div className='rounded-lg  md:w-1/2'>
           <ul>
-            <li className='border-b-2'>Business Strategy</li>
-            <li className='border-b-2'>Content Creation</li>
-            <li className='border-b-2'>Branding</li>
-            <li className='border-b-2'>Marketing</li>
-            <li className='border-b-2'>CONSULTING</li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              Business Strategy
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              Content Creation
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              Branding
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              Marketing
+            </li>
+            <li className='border-b-2'>
+              <SquiggleIcon />
+              CONSULTING
+            </li>
           </ul>
         </div>
       </div>
--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 import styles from './Work.module.css';
 
 import Button from '@/components/Button/Button';
+import SquiggleIcon from '@/components/SquiggleIcon/SquiggleIcon';
 
 export default function Work() {
   return (
     <div className={styles.WorkComponent + ' relative'}>
       {/* Work */}
       <div className='mx-auto bg-black px-4 py-16 text-center'>
+        <SquiggleIcon />
         <p className='font-primary  my-8  px-4 text-5xl uppercase text-white'>
           CASE STUDIES
         </p>

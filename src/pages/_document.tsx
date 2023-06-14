1986e72e31be37424d6290f3aa2d257046c82256
Maria Saavedra <msaav3@gmail.com>
2023-06-14 00:05:20 -0500

    chore: fix gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head></Head>
-      <Script
-        src='https://www.googletagmanager.com/gtag/js?id=G-89Z9D0T85Q'
-        strategy='afterInteractive'
-      />
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-89Z9D0T85Q');
-    console.log('gtag init');
-  `}
-      </Script>
       <body>
         <Main />
         <NextScript />

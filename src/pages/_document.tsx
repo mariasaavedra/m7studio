b718b3302e9fdb10f9e90dd562864d7ac0600778
Maria Saavedra <msaav3@gmail.com>
2023-06-13 23:03:46 -0500

    chore: ga analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -12,6 +13,18 @@ export default function Document() {
           crossOrigin='anonymous'
         /> */}
       </Head>
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-89Z9D0T85Q'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-89Z9D0T85Q');
+  `}
+      </Script>
       <body>
         <Main />
         <NextScript />

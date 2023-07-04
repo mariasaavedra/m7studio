a6f7dba4ab34a75eb12a2923bdbc826b252848d8
Maria Saavedra <msaav3@gmail.com>
2023-07-04 14:00:39 -0500

    chore: updated weight

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
           <strong className='text-xl'>M7 STUDIO </strong> is a collective of
           imaginative individuals, including creatives, designers, and
           engineers, operating from our headquarters in
-          <span className='font-normal italic'>Kansas City, MO</span>. Our
+          <span className='font-normal italic'> Kansas City, MO</span>. Our
           portfolio boasts esteemed collaborations with renowned brands such as
           <span className='font-normal italic'>
             Sporting KC, Chicago Fire, LA Galaxy, Standford, Museum of Design
